f607c3726dadf63c26b67af62608c376ac91f956
Null <pairmanxlr@gmail.com>
2024-12-05 18:52:14 +0800

    Create FuckItWeButt.user.js

---
+++ FuckItWeButt.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         FuckItWeButt
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Unhide "Download" & "Save" Buttons on Chaoxing Clouddisk Share Pages for "suspicious" filetypes.
+// @author       Pairman <pairmanxlr@gmail.com>
+// @match        https://pan-yz.chaoxing.com/external/m/file/*
+// @match        https://pan-yz.cldisk.com/external/m/file/*
+// @supportURL   https://github.com/Pairman/FuckItWeButt
+// @license      GPLv3
+// ==/UserScript==
+
+(function() {
+	'use strict';
+
+	$(document).ready(() => {
+		$.ajax({
+			url: `https://noteyd.chaoxing.com/screen/note_note/files/status/${fileinfo.objectId}`,
+			success: d => fileinfo.download = d.download
+		});
+
+		$('#reportFileBtn').before(
+			'<a id="download"><span class="YpBtn">下载</span></a>' +
+			'<a id="saveyp"><span class="YpBtn yellowBg">保存到云盘</span></a>'
+		);
+		$('<script>').text($('body > script:nth-child(5)').text()).appendTo('body');
+	});
+})();
